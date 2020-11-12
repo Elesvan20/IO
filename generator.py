8d0dc411da6c95e86acd44136deefef5baa415dd
Elesvan <kevinledezma20498@gmail.com>
2020-11-12 11:14:21 -0600

    modificaciones visuales

--- generator.py
+++ generator.py
@@ -4,53 +4,42 @@
     Profesor: Carlos Gamboa Venegas
     II Semestre 2020
 '''
-########################################################################################################################
-'''
-Input:
-    python3 generator.py W N minPeso maxPeso minBeneficio maxBeneficio minCantidad maxCantidad
-    donde
-    W = peso maximo de la mochila
-    N = cantidad maxima de objetos
-Output:
-    beneficio
-    articulo i, cantidad
-    articulo i+1, cantidad
-    .
-    .
-    .
-    articulo n, cantidad
-'''
+
+#
+#Input:
+#    python3 generator.py W N minPeso maxPeso minBeneficio maxBeneficio minCantidad maxCantidad
+#    donde
+#    W = peso maximo de la mochila
+#    N = cantidad maxima de objetos
+#Output:
+#    beneficio
+#    articulo i, cantidad
+#    articulo i+1, cantidad
+#    .
+#    .
+#    .
+#    articulo n, cantidad
+#
 
 
 def problema1_mochila():
     print("Resuelvo el problema de la mochila")
 
 
-########################################################################################################################
-'''
-Sintaxis:
-python3 generator.py largoH1 largoH2
-
-♦♦♦SOLO UTILIZAR LETRAS A T C G♦♦♦
-
-Input:
-    linea1: tipo de scoring: match, missmatch, gap
-    linea2: Hilera1
-    linea3: Hilera2
-
-Ejemplo:
-    1,-1,-2
-    ATTGTGATCC
-    TTGCATCGGC
-
-Output:
-    Tabla de resultados [matriz]
-    
-    Puntaje Final: -2
-    Hilera1: ATTGTGATC__C
-    Hilera2: _TTG_CATCGGC
-    
-'''
+#
+#Sintaxis:
+#python3 generator.py largoH1 largoH2
+#
+#♦♦♦SOLO UTILIZAR LETRAS A T C G♦♦♦
+#
+#
+#
+#Output:
+#    Tabla de resultados [matriz]
+#
+#    Puntaje Final: -2
+#    Hilera1: ATTGTGATC__C
+#    Hilera2: _TTG_CATCGGC
 
 
 def problema2_alineamiento():
--- solver.py
+++ solver.py
@@ -6,7 +6,34 @@
 '''
 
 
+def fuerza_bruta():
+    print("Fuerza Bruta")
+
+
+def programacion_dinamica():
+    print("progra dinamica")
+
+
+#
+#    Obtiene los parametros del archivo indicado, en caso de recibir un archivo de entrada
+#
+#
+def obtener_parametros():
+    print("obtener parametros")
+
 def main():
     print("Hello World")
 
 main()
+'''
+    Sintaxis
+    python3 solver.py [-h] [archivo]
+    
+    Sintaxis para analizar problemas por medio de archivos:
+    linea1: peso maximo de la mochila
+    linea2: elemento i (peso, beneficio, cantidad)
+    .
+    .
+    .
+    linea n: elemento n (peso, beneficio, cantidad)
+'''
