b05f053bc777b0dada4d4643732d6a92cbc3a353
Elesvan <kevinledezma20498@gmail.com>
2020-11-12 10:02:25 -0600

    Se crea el archivo principal de ejecución

---
+++ solver.py
@@ -0,0 +1,12 @@
+'''
+    Proyecto 2 - Investigacion de Operaciones
+    Estudiante: Kevin Ledezma Jimenez
+    Profesor: Carlos Gamboa Venegas
+    II Semestre 2020
+'''
+
+
+def main():
+    print("Hello World")
+
+main()
