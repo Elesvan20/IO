1280efd142691e4a63a218cce28ce0ec183bf22b
Elesvan <kevinledezma20498@gmail.com>
2020-12-13 15:26:52 -0600

    Se agrega la obtencion de informacion para el generador

--- generator.py
+++ generator.py
@@ -21,6 +21,23 @@
 #    articulo n, cantidad
 #
 
+import sys
+
+#Variables globales
+problema = 0
+parametros = ""
+nombre_archivo = ""
+
+#Codigos posibles errores
+POCOSPARAMETROS = -1
+MUCHOSPARAMETROS = -2
+ARCHIVONOENCONTRADO = -3
+PROBLEMAINVALIDO = -4
+ALGORITMOINVALIDO = -5
+
+
+ERROR_ARCHIVO = "\nERROR: Parece que el archivo indicado no existe dentro de mi carpeta de ejecucion!!\n"
+SINTAXIS_USO = "\nSintaxis: python3 generator.py PROBLEMA(1/2) ARCHIVO(nombre.txt) PARAMETROS (una sola instruccion, usar comillas)\n"
 
 def problema1_mochila():
     print("Resuelvo el problema de la mochila")
@@ -46,16 +63,55 @@ def problema2_alineamiento():
     print("Resuelvo el problema de alineamiento de secuencias")
 
 ########################################################################################################################
+'''
+    Funcion principal de obtener los parametros
+    desde la consola a las variables para 
+    configurar los ejercicios
+'''
+def obtener_parametros():
+    global problema, parametros, nombre_archivo
+    '''
+        Casos especiales de entrada
+    '''
+    # casos especiales de error
+    if (len(sys.argv) < 4):
+        print("Muchos argumentos recibidos: "+str(len(sys.argv))+"\n"+SINTAXIS_USO)
+        sys.exit(POCOSPARAMETROS)
 
+    if (len(sys.argv) > 4):
+        print("Muchos argumentos recibidos: "+str(len(sys.argv))+"\n"+SINTAXIS_USO)
+        sys.exit(MUCHOSPARAMETROS)
+
+    #Se captura el parametro introducido en consola como configuracion de problema
+    problema = int(sys.argv[1])
+
+    #Verificamos que sea un valor permitido
+    if (0 > problema and problema > 2):
+        print("ERROR: Valor de problema invalido! Debe ser 1 o 2 \n"
+              "Se recibio un "+str(problema))
+        sys.exit(PROBLEMAINVALIDO)
+
+    # Guardamos el nombre para el futuro archivo de texto
+    nombre_archivo = sys.argv[2]
+
+    #Guardamos en un arreglo los datos recibidos como parametros
+    parametros = sys.argv[3].split(" ")
 
 def main():
-    print("Hello World Im the Generator")
+    #invocamos la obtencion de los datos
+    obtener_parametros()
+    print("Usando algoritmo: " + str(problema))
+    print("Nombre deseado para el archivo " + str(nombre_archivo))
+    print("Parametros obtenidos: ", parametros)
 
 
+main()
 '''
     Sintaxis
-    python3 generator.py ID-PROBLEMA [parametros]
+    python3 generator.py PROBLEMA ARCHIVO PARAMETROS
     
-    donde
-    ID-PROBLEMA es requerido obligatoriamente
+    Donde
+    PROBLEMA es 1 (mochila) o 2 (alineamiento)
+    ARCHIVO es el nombre donde se escribe la salida del ejercicio
+    PARAMETROS todo lo requerido segun el ejercicio
 '''
